3eef62945673a7a89053c84dbe445971836ca8e0
Devraj Srivastawa <100594605+Devraj5032@users.noreply.github.com>
2023-12-26 12:11:37 +0530

    cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-
+const cors = require('cors')
 
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(bodyParser.json());
 
 app.post('/process-image', async (req, res) => {
