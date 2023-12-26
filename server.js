e10acdbeee8bafd92528f145e4fc55f2681e8398
Devraj Srivastawa <100594605+Devraj5032@users.noreply.github.com>
2023-12-26 11:44:34 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/process-image', async (req, res) => {
+  if(req.body.token !== procecss.env.TOKEN){
+    return res.send({error:"auth failed, token not varified"})
+  }
+ axios({
+  method:"POST",
+  url: "https://detect.roboflow.com/garbage-detector-ccn0g/1",
+  params: {
+    api_key: process.env.API_KEY,
+    image: req.body.imageUrl
+}}).then(function(response) {
+  const data = response.data 
+  if(data.predictions.length>0){
+   return res.send({cleaned:false})
+  }else{
+   return res.send({cleaned:true})
+  }
+})
+.catch(function(error) {
+  console.log(error.message);
+});
+}
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
